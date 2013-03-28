c7ad3d219049fc335b6ee07de815d85e27889d55
Bryce <bcfrye19@gmail.com>
2013-03-27 20:02:39 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chefly::Application.config.secret_token = '47d7107bbad68652406ac7d2afbfd447b4a3fc76ff76cac36cd69f55916cae570ae616141faea5f88f641931488d99653f3fb8fc502927923a3c7638e7048708'
